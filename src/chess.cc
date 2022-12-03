849612e2aa787b8498647ec4094ea36eb408219d
VictorWu03 <71724675+VictorWu03@users.noreply.github.com>
2022-12-03 00:17:25 -0500

    chess may be done

--- src/chess.cc
+++ src/chess.cc
@@ -14,15 +14,6 @@
 
 using namespace std;
 
-// Chess::Chess(Board* curBoard, Board* prevBoard, string curPlayer, Player* white, Player* black, bool gameRunning, 
-//             int result, int whiteWin, int blackWin): curBoard{curBoard}, prevBoard{prevBoard}, curPlayer{curPlayer}, 
-//             white{white}, black{black}, gameRunning{false}, result{-1}, whiteWin{0}, blackWin{0} {}
-
-// Chess::Chess():
-//     gameRunning{false}, whiteWin{0}, blackWin{0}
-// {}
-
-
 Chess::~Chess() {
     delete curBoard;
     delete prevBoard;
@@ -30,11 +21,12 @@ Chess::~Chess() {
     delete black;
 }
 
+
 void Chess::takeTurn() {
     string command;
     while (cin >> command) {
         if (command == "game") {
-            if (gameRunning) {
+            if (gameStart) {
                 cout << "invalid command, game in progress" << endl;
                 continue;
             }
@@ -44,13 +36,16 @@ void Chess::takeTurn() {
 
             if ((whiteP == "human" || whiteP == "computer1" || whiteP == "computer2" || whiteP == "computer3" || whiteP == "computer4") && 
             (blackP == "human" || blackP == "computer1" || blackP == "computer2" || blackP == "computer3" || blackP == "computer4")) {
+                gameStart = true;
+                gameRunning = false;
+
                 delete prevBoard;
                 delete curBoard;
                 delete white;
                 delete black;
                 prevBoard = nullptr;
                 curBoard = new Board{};
-                gameRunning = true;
+                
                 curPlayer = "white";
                 
                 if (whiteP == "human") {
@@ -77,15 +72,17 @@ void Chess::takeTurn() {
                 // } else {
                 //     black = new Computer4{"black"};
                 // }
+                notifyObservers();
             } else {
                 cout << "invalid command, unknown player type" << endl;
             }
         // end of "game"
 
         } else if (command == "resign") {
-            if (!gameRunning) {
+            if (!gameStart) {
                 cout << "invalid command, no game in progress" << endl;
             }
+            
             if (curPlayer == "white") {
                 blackWin++;
                 cout << "Black wins!" << endl;
@@ -93,199 +90,181 @@ void Chess::takeTurn() {
                 whiteWin++;
                 cout << "White wins!" << endl;
             }
-            delete curBoard;
-            delete prevBoard;
-            delete white;
-            delete black;
-            curBoard = nullptr;
-            prevBoard = nullptr;
-            white = nullptr;
-            black = nullptr;
-            gameRunning = false;
+            
+            reset();
         // end of "resign"
 
         } else if (command == "move") {
-            if (!gameRunning) {
+            if (!gameStart) {
                 cout << "invalid command, cannot move when game is not running" << endl;
+                continue;
             }
 
+            gameRunning = true;
             delete prevBoard;
             prevBoard = curBoard;
 
             if (curPlayer == "white") {
                 curBoard = white->move(prevBoard);
-                // checkmate, check, stalemate
-                // should the curboard, prevboard, and players be deleted if gameRunning is false
-                if (curBoard->checkmate("black")) {
-                    cout << "Checkmate! White wins!" << endl;
-                    whiteWin++;
-                    gameRunning = false;
-                } else if (curBoard->check("black")) {
-                    cout << "Black is in check." << endl;
-                } else if (curBoard->stalemate("black")) {
-                    cout << "Stalemate!" << endl;
-                    whiteWin += 0.5;
-                    blackWin += 0.5;
-                }
-
             } else {
+                // curPlayer == "black"
                 curBoard = black->move(prevBoard);
-                // checkmate, check, stalemate
-                if (curBoard->checkmate("white")) {
-                    cout << "Checkmate! Black wins!" << endl;
-                    whiteWin++;
-                    gameRunning = false;
-                } else if (curBoard->check("white")) {
-                    cout << "White is in check." << endl;
-                } else if (curBoard->stalemate("white")) {
-                    cout << "Stalemate!" << endl;
-                    whiteWin += 0.5;
-                    blackWin += 0.5;
-                }
             }
-
-            // notify observers after move
-            notifyObservers();
-
 
             // switch player
             curPlayer = curPlayer == "white" ? "black" : "white";
+            // notify observers after move
+            notifyObservers();
+            bool terminate = false;
 
-            // check if stalemate/checkmate/check && display message
+            if (curBoard->checkmate("black")) {
+                cout << "Checkmate! White wins!" << endl;
+                whiteWin++;
+                terminate = true;
+            } else if (curBoard->checkmate("white")) {
+                cout << "Checkmate! Black wins!" << endl;
+                blackWin++;
+                terminate = true;
+            } else if (curBoard->check("black")) {
+                cout << "Black is in check." << endl;
+            } else if (curBoard->check("white")) {
+                cout << "White is in check." << endl;
+            } else if (curBoard->stalemate("black") || curBoard->stalemate("white")) {
+                cout << "Stalemate!" << endl;
+                whiteWin += 0.5;
+                blackWin += 0.5;
+                terminate = true;
+            }
+
+            if (!gameRunning) {
+                reset();
+            }
         // end of "move"
 
         } else if (command == "setup") {
-            if (gameRunning) {
+            if ((gameStart == true) && (gameRunning == false)) {
+                string pos;
+                char type;
+                int row;
+                int col;
+
+                while (cin >> command) {
+                    if (command == "+") {
+                        cin >> type >> pos;
+                        if ('a' <= pos[0] && pos[0] <= 'h' && '1' <= pos[1] && pos[1] <= '8') {
+                            row = pos[1] - '1';
+                            col = pos[0] - 'a';
+                            Piece *oldp = curBoard->theBoard[row][col];
+                            // pointer to [row][col]
+                            Piece **p = &curBoard->theBoard[row][col];
+
+                            if (type == 'K') {
+                                *p = new King(row, col, "white", false, false);
+                            } else if (type == 'Q') {
+                                *p = new Queen(row, col, "white", false, false);
+                            } else if (type == 'R') {
+                                *p = new Rook(row, col, "white", false, false);
+                            } else if (type == 'B') {
+                                *p = new Bishop(row, col, "white", false, false);
+                            } else if (type == 'N') {
+                                *p = new Knight(row, col, "white", false, false);
+                            } else if (type == 'P') {
+                                *p = new Pawn(row, col, "white", false, false, false);
+                            } else if (type == 'k') {
+                                *p = new King(row, col, "black", false, false);
+                            } else if (type == 'q') {
+                                *p = new Queen(row, col, "black", false, false);
+                            } else if (type == 'r') {
+                                *p = new Rook(row, col, "black", false, false);
+                            } else if (type == 'b') {
+                                *p = new Bishop(row, col, "black", false, false);
+                            } else if (type == 'n') {
+                                *p = new Knight(row, col, "black", false, false);
+                            } else if (type == 'p') {
+                                *p = new Pawn(row, col, "black", false, false, false);
+                            } else {
+                                cout << "invalid command, unknown type" << endl;
+                                // put oldp back to board
+                                *p = oldp;
+                                continue;
+                            }
+                            // free oldp here, after knowing type is valid
+                            delete oldp;
+                            // notify observers after sucess "+"
+                            notifyObservers();
+                        } else {
+                            cout << "invalid command, pos is out of range" << endl;
+                        }
+                    // end of "+"
+
+                    } else if (command == "-") {
+                        cin >> pos;
+                        if ('a' <= pos[0] && pos[0] <= 'h' && '1' <= pos[1] && pos[1] <= '8') {
+                            row = pos[1] - '1';
+                            col = pos[0] - 'a';
+                            // [row][col] is either Piece or nullptr, delete is fine eitherway
+                            delete curBoard->theBoard[row][col];
+                            curBoard->theBoard[row][col] = nullptr;
+                            notifyObservers();
+                        } else {
+                            cout << "invalid command, move is out of range" << endl;
+                        }
+                    // end of "-"
+
+                    } else if (command == "=") {
+                        string team;
+                        cin >> team;
+                        if (team == "white" || team == "black") {
+                            curPlayer = team;
+                        } else {
+                            cout << "invalid command, unknown colour" << endl;
+                        }
+                    // end of "="
+                    
+                    } else if (command == "done") {
+                        // check for validboard after setup is compelete
+                        if (!(curBoard->validBoard())) {
+                            cout << "invalid board" << endl;
+                            continue;
+                        }
+
+                        notifyObservers();
+
+                        // upon leaving setup mode, set all piece moved to be true, AKA disable castle and enpassant
+                        for (int i = 0; i < 8; i++) {
+                            for (int j = 0; j < 8; j++) {
+                                Piece *p = curBoard->theBoard[i][j];
+                                if (p == nullptr) {
+                                    continue;
+                                }
+                                p->setMoved(true);
+                            }
+                        }
+                        // if board is stalemate on current team, output message, increase win count, playing = false
+                        if (curBoard->stalemate(curPlayer)) {
+                            cout << "Stalemate!" << endl;
+                            whiteWin += 0.5;
+                            blackWin += 0.5;
+                            reset();
+                        }
+                        break;
+                    // end of "done"
+                    } else {
+                        cout << "invalid command, unknown setup command" << endl;
+                    }
+                // end of "setup command loop"
+                }
+            } else {
                 cout << "invalid command, cannot enter set-up mode when a game is in progress" << endl;   
                 continue;
             }
-
-            string pos;
-            char type;
-            int row;
-            int col;
-
-            while (cin >> command) {
-                // checkif the current square is occupied
-                if (command == "+") {
-                    cin >> type >> pos;
-                    if ('a' <= pos[0] && pos[0] <= 'h' && '1' <= pos[1] && pos[1] <= '8') {
-                        row = pos[1] - '1';
-                        col = pos[0] - 'a';
-                        // always delete [row][col], ok if [row][col]==nullptr
-                        Piece *oldp = curBoard->theBoard[row][col];
-                        // pointer to [row][col]
-                        Piece **p = &curBoard->theBoard[row][col];
-
-                        if (type == 'K') {
-                            *p = new King(row, col, "white", false, false);
-                        } else if (type == 'Q') {
-                            *p = new Queen(row, col, "white", false, false);
-                        } else if (type == 'R') {
-                            *p = new Rook(row, col, "white", false, false);
-                        } else if (type == 'B') {
-                            *p = new Bishop(row, col, "white", false, false);
-                        } else if (type == 'N') {
-                            *p = new Knight(row, col, "white", false, false);
-                        } else if (type == 'P') {
-                            *p = new Pawn(row, col, "white", false, false, false);
-                        } else if (type == 'k') {
-                            *p = new King(row, col, "black", false, false);
-                        } else if (type == 'q') {
-                            *p = new Queen(row, col, "black", false, false);
-                        } else if (type == 'r') {
-                            *p = new Rook(row, col, "black", false, false);
-                        } else if (type == 'b') {
-                            *p = new Bishop(row, col, "black", false, false);
-                        } else if (type == 'n') {
-                            *p = new Knight(row, col, "black", false, false);
-                        } else if (type == 'p') {
-                            *p = new Pawn(row, col, "black", false, false, false);
-                        } else {
-                            cout << "invalid command, unknown type" << endl;
-                            // put oldp back to board
-                            *p = oldp;
-                            continue;
-                        }
-                        // free oldp here, after knowing type is valid
-                        delete oldp;
-                    } else {
-                        cout << "invalid command, move is out of range" << endl;
-                    }
-                // end of "+"
-
-                } else if (command == "-") {
-                    cin >> pos;
-                    if ('a' <= pos[0] && pos[0] <= 'h' && '1' <= pos[1] && pos[1] <= '8') {
-                        row = pos[1] - '1';
-                        col = pos[0] - 'a';
-                        // [row][col] is either Piece or nullptr, delete is fine eitherway
-                        delete curBoard->theBoard[row][col];
-                        curBoard->theBoard[row][col] = nullptr;
-                    } else {
-                        cout << "invalid command, move is out of range" << endl;
-                    }
-                // end of "-"
-
-                } else if (command == "=") {
-                    string team;
-                    cin >> team;
-                    if (team == "white" || team == "black") {
-                        curPlayer = team;
-                    } else {
-                        cout << "invalid command, unknown colour" << endl;
-                    }
-                // end of "="
-                
-                } else if (command == "done") {
-                    // check for validboard after setup is compelete
-                    if (!(curBoard->validBoard())) {
-                        cout << "invalid board" << endl;
-                        continue;
-                    }
-                    // upon leaving setup mode, set all piece moved to be true, AKA disable castle and enpassant
-                    for (int i = 0; i < 8; i++) {
-                        for (int j = 0; j < 8; j++) {
-                            Piece *p = curBoard->theBoard[i][j];
-                            if (p == nullptr) {
-                                continue;
-                            }
-                            p->setMoved(true);
-                        }
-                    }
-
-
-                    // if board is stalemate on current team, output message, increase win count, playing = false
-                    if (curBoard->stalemate(curPlayer)) {
-                        cout << "Stalemate!" << endl;
-                        whiteWin += 0.5;
-                        blackWin += 0.5;
-                        gameRunning = false;
-                    } else {
-                        gameRunning = true;
-                    }
-
-                    // else playing = true
-                    break;
-
-                    
-                // end of "done"
-
-                } else {
-                    cout << "invalid command, unknown setup command" << endl;
-                }
-            // end of "setup command loop"
-            }
         // end of "setup"
+
         } else if (command == "undo") {
             // check if game is running
             if (gameRunning) {
                 if (prevBoard != nullptr) {
-                    if (curPlayer == "white") {
-                        curPlayer = "black";
-                    } else {
-                        curPlayer = "white";
-                    }
+                    curPlayer = curPlayer == "white" ? "black" : "white";
                     delete curBoard;
                     curBoard = prevBoard;
                     prevBoard = nullptr;
@@ -302,6 +281,20 @@ void Chess::takeTurn() {
 }
 
 
-Board & Chess::getState() {
+Board& Chess::getState() {
     return *curBoard;
+}
+
+
+void Chess::reset() {
+    gameRunning = false;
+    gameStart = false;
+    delete curBoard;
+    delete prevBoard;
+    delete white;
+    delete black;
+    curBoard = nullptr;
+    prevBoard = nullptr;
+    white = nullptr;
+    black = nullptr;
 }
--- src/chess.h
+++ src/chess.h
@@ -1,14 +1,18 @@
 #ifndef CHESS_H_
 #define CHESS_H_
 
-#include "board.h"
-#include "player.h"
+// #include "board.h"
+// #include "player.h"
 #include "subject.h"
 #include <string>
+
+class Player;
+class Board;
 
 class Chess: public Subject {
     std::string curPlayer;
     bool gameRunning;
+    bool gameStart;
     Board* curBoard;
     Board* prevBoard;
     Player* white;
@@ -16,13 +20,12 @@ class Chess: public Subject {
     // int result; // result 0 = white win, 1 = blackwin
     float whiteWin;
     float blackWin;
+    void reset();
 public:
     // Chess(Board* curBoard, Board* prevBoard, std::string curPlayer, Player* white, Player* black, bool gameRunning, int result, int whiteWin, int blackWin);
     // Chess();
     ~Chess();
     void takeTurn();
-    // undo is a command, does not require its own functino
-    void undo();
     Board & getState();
 };
 
